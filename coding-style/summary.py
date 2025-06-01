d607d24c0ad52d94f3264707d1399c98fb485ce0
Cavin <cavinlarry001@gmail.com>
2025-06-01 18:36:29 +0300

    Revised on the coding style guide(PEP8)

---
+++ coding-style/summary.py
@@ -0,0 +1,8 @@
+"""
+The length of a line of code shouldn't exceed 79 characters.
+
+Extra spaces should be avoided within parentheses and before an opening parenthesis.
+
+Quotes should be used consistently or to avoid backslashes
+
+"""
