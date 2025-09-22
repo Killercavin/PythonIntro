2eaa76b558a44df6ecbbe60dbc7066b7abef13a6
Cavin <cavinlarry001@gmail.com>
2025-09-22 20:42:30 +0300

    feat: add initial implementations for list, scope, tracing, and test modules

---
+++ debugging/scope.py
@@ -0,0 +1,7 @@
+def change_value(b: int) -> int:
+  b = 5
+  return b
+
+a = 7
+change_value(a)
+print(a)
---
+++ test/test0.py
@@ -0,0 +1,4 @@
+def calculate(x: int) -> int:
+    return 3 * x + 4
+
+assert(calculate(0), 4)
