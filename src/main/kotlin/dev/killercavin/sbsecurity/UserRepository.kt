e2d704b82f4f63051d618a8508af376b3788c5da
Cavin <cavinlarry001@gmail.com>
2026-04-26 11:54:41 +0300

    chore: Refactor repository

--- src/main/kotlin/dev/killercavin/sbsecurity/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package dev.killercavin.sbsecurity
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository: JpaRepository<User, Long> {
-    fun findByUsername(username: String): User?
-    fun existsUserByUsername(username: String): Boolean
-}
