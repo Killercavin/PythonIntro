ea217786e11b4e26d62b9a8c905fbd978e1a0808
Cavin <cavinlarry001@gmail.com>
2026-05-01 23:26:02 +0300

    delete user service

--- user-service/src/main/java/io/github/devcavin/wattwise/userservice/dto/UserDto.java
+++
@@ -1,9 +0,0 @@
-package io.github.devcavin.wattwise.userservice.dto;
-
-public record UserDto(
-        Long id,
-        String fullName,
-        String email,
-        boolean alerting,
-        double alertingThreshold
-) { }
